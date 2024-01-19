82b00aed038e3232ad55e8ff9340c1082cc99d33
Jhonathan.Esguerra <Jhonathan.Esguerra@iwgplc.com>
2024-01-19 12:32:10 +0100

    Se realizan cambios en el header, debido a cambios en el estilo de la maquetación

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,14 +9,14 @@ function App() {
           <div className='header_div'>
             <nav>
               <a href="#"> Home </a>
-              <a href="#"> About me </a>
-              <a href="#"> Projects </a>
+              <a href="#"> Sobre mi </a>
+              <a href="#"> Proyectos </a>
               <a href="#"> Other </a>
-              <a href="#"> Social Networks </a>
+              <a href="#"> Contacto </a>
             </nav>
           </div>
-          
-          <section className='banner'>
+          <div className='header_banner'>
+            <section className='banner'>
             <section className='banner_title'>
               <h1> Darling Rayran Castro </h1>
               <h2> Full Stack Developer </h2>
@@ -29,9 +29,12 @@ function App() {
               <li><i class="fas fa-database"></i></li>
               <li><i class="fab fa-github"></i></li>
             </ul>
-            <a href="#" className='contact'> Contact me </a>
-          </section>
+            <a href="#" className='cv'> Download CV </a>
+            </section>
+          </div>
+      </header>
 
+      <body>
         <div className='burbujas'>
             <div className='burbuja'></div>
             <div className='burbuja'></div>
@@ -42,24 +45,9 @@ function App() {
             <div className='burbuja'></div>
             <div className='burbuja'></div>
         </div>
-
-      </header>
-      <body>
-        <div className="page">
-          <h1 className="title1">About me </h1> 
-          <h2> Proje </h2>
-          <h2></h2>
-          <p>probando</p>
-          <ul>
-            <li>started</li>
-            <li>kit</li>
-            <li>react</li>
-          </ul>
-        </div>
     </body>
 
     <footer>
-      <img src="./src/images/logoDR.png" alt="Logo" />
     </footer>
   </div>
   );
