284897195630f46803e3e3141db5ab1f039fd309
Jhonathan.Esguerra <Jhonathan.Esguerra@iwgplc.com>
2024-02-08 22:55:11 +0100

    se ajusta ruta de acceso principal a banner

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 /* componente principal de nuestra web */
-import '../styles/App.scss'; 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import '../styles/App.scss';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Menu from './header/menu';
 import SubMenu from './header/subMenu';
 import Animation from './body/animation';
@@ -10,26 +10,25 @@ import Projects from './body/projects';
 import Certifications from './body/certifications';
 import Form from './body/form';
 
-
-
 function App() {
   return (
-    <Router>
-      <div> 
-        < Menu />
-        < SubMenu />
-        < Animation />
+    <Router basename="/portfolioDarlingRayran">
+      <div>
+        <Menu />
+        <SubMenu />
+        <Animation />
         <Routes>
-          <Route path="/banner" element={< Banner /> } />
-          <Route path="/about" element={ < About /> } />
-          <Route path="/projects" element={< Projects />} />
-          <Route path="/certifications" element={<Certifications/>} />
-          <Route path="/contact" element={< Form/>} />
+          <Route path="/" element={<Navigate replace to="/banner" />} />
+          <Route path="/banner" element={<Banner />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/certifications" element={<Certifications />} />
+          <Route path="/contact" element={<Form />} />
         </Routes>
-    </div>
-  </Router>
-
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
